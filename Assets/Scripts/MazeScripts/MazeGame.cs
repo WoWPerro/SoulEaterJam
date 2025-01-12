39d1d3a4d276b3625f2e0c17ee2f98c2531bef35
saseb <sasebrev@gmail.com>
2025-01-11 18:05:06 -0600

    Cummit
    Assets y cosas en la Jerarquia

---
+++ Assets/Scripts/MazeScripts/MazeGame.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MazeGame : MonoBehaviour{
+
+    [SerializeField] private Transform startPosition;
+    
+
+    
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
